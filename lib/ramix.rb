0d75a1b130d912ab5c58d80047072dfbdc7165b7
Thierry Zires <zshuaibin@gmail.com>
2012-01-12 16:56:50 +0800

    Code refactoring

---
+++ lib/ramix.rb
@@ -0,0 +1,11 @@
+require 'rubygems' if RUBY_VERSION < '1.9'
+
+begin
+  require 'rails/version'
+  require 'rails/generators'
+  require 'rails/generators/rails/app/app_generator'
+rescue LoadError
+  puts "*Note* Rails is not available. Type `gem install rails` to install rails"
+end
+
+require 'ramix/app_generator'
--- lib/ramix/commands.rb
+++ lib/ramix/commands.rb
@@ -1,15 +1,5 @@
-require 'rubygems' #TODO ruby1.9 do not need require rubygems
-begin
-  require 'rails/version'
-  require 'rails/generators'
-  require 'rails/generators/rails/app/app_generator'
-rescue LoadError
-  puts "*Note* Rails is not available. Type `gem install rails` to install rails"
-end
-require 'ramix/app_generator'
-require 'ramix/template_generator'
-
 if ARGV.first == "new"
   ARGV.shift
 end
+require 'ramix'
 Ramix::AppGenerator.start
