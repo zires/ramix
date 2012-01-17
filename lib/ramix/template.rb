d0cc67824112b09f9912f4b702997c2e25077e11
Thierry Zires <zshuaibin@gmail.com>
2012-01-17 15:38:23 +0800

    Code format

--- lib/ramix/template.rb
+++ lib/ramix/template.rb
@@ -8,30 +8,30 @@ module Ramix
     SELF_ATTRIBUTE    = %w(dependence)
     ATTRIBUTE         = THOR_CLASS_OPTION + SELF_ATTRIBUTE
 
-  	def initialize(name, path = nil)
-  		@name = name
-  		begin
-				draft, attribute, @output = File.read( File.join(DIR_PATH, name + '.rb') ).split('---')
-				@attribute                = YAML.load(attribute)
-  		rescue
-				@attribute, @output       = {}, "\n\ngem '#{name}'"
-  		end
+    def initialize(name, path = nil)
+      @name = name
+      begin
+        draft, attribute, @output = File.read( File.join(DIR_PATH, name + '.rb') ).split('---')
+        @attribute                = YAML.load(attribute)
+      rescue
+        @attribute, @output       = {}, "\n\ngem '#{name}'"
+      end
     end
 
     def name
-    	@attribute['name'] || @name
+      @attribute['name'] || @name
     end
 
     ATTRIBUTE.each do |attr|
-    	class_eval <<-RUBY
-    		def #{attr}
+      class_eval <<-RUBY
+        def #{attr}
           @attribute['#{attr}']
         end
-    	RUBY
+      RUBY
     end
     
     def output
-    	"# >====================== [#{name}] =======================<" + @output
+      "# >====================== [#{name}] =======================<" + @output
     end
 
   end
