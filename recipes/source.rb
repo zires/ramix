a75aec94eab2e30c020d08b7ba47a801f713174f
Thierry Zires <zshuaibin@gmail.com>
2012-01-18 16:41:39 +0800

    Do not need source option

--- recipes/source.rb
+++
@@ -1,9 +0,0 @@
----
-name: 'source'
-desc: 'Special the source path of Gemfile.'
-default: 'http://rubygems.org'
-aliases: '-S'
-type: 'string'
----
-
-gsub_file 'Gemfile', "source 'http://rubygems.org'", "source '#{@source}'"
